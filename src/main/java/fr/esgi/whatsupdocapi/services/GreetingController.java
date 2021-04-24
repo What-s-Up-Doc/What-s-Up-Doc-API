f4b7dd08667b031383286630b814199f057b9fe3
Gwendal <gwendou95@hotmail.fr>
2021-04-25 00:52:01 +0200

    fix: adapt to release on main

--- src/main/java/fr/esgi/whatsupdocapi/services/GreetingController.java
+++ src/main/java/fr/esgi/whatsupdocapi/services/GreetingController.java
@@ -11,17 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private static final String template2 = "Test, %s!";
     private final AtomicLong counter = new AtomicLong();
-    private final AtomicLong counterDeploy = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-    @GetMapping("/deploy")
-    public Greeting deploy(@RequestParam(value = "name", defaultValue = "Deploy") String name) {
-        return new Greeting(counterDeploy.incrementAndGet(), String.format(template2, name));
-    }
 }

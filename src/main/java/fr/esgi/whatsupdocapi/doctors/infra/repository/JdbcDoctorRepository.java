ec782a0935c4ce88773b3ed211b8b082007818d7
Pinklady <katyanne_mm@live.fr>
2021-05-04 23:18:45 +0200

    fix: update jdbc table name doctor

--- src/main/java/fr/esgi/whatsupdocapi/doctors/infra/repository/JdbcDoctorRepository.java
+++ src/main/java/fr/esgi/whatsupdocapi/doctors/infra/repository/JdbcDoctorRepository.java
@@ -19,36 +19,36 @@ public class JdbcDoctorRepository implements DoctorRepository {
     @Override
     public String store(String firstname, String lastname, String email, String password, String phone, String gender, String speciality) {
         var id = UUID.randomUUID().toString();
-        jdbcTemplate.update("INSERT INTO doctors (id, firstname, lastname, email, password, phone, gender, speciality) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", id, firstname, lastname, email, password, phone, gender, speciality);
+        jdbcTemplate.update("INSERT INTO doctor (id, firstname, lastname, email, password, phone, gender, speciality) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", id, firstname, lastname, email, password, phone, gender, speciality);
         return id;
     }
 
     @Override
     public int count() {
-        return jdbcTemplate.queryForObject("select count(*) from doctors", Integer.class);
+        return jdbcTemplate.queryForObject("select count(*) from doctor", Integer.class);
     }
 
     @Override
     public List<Doctor> findAll() {
-        return jdbcTemplate.query("select * from doctors", mapper);
+        return jdbcTemplate.query("select * from doctor", mapper);
     }
 
     @Override
     public Optional<Doctor> findOne(String doctorId) {
         return Optional.ofNullable(
-                jdbcTemplate.queryForObject("select * from doctors where id = ?", mapper, new Object[]{ doctorId })
+                jdbcTemplate.queryForObject("select * from doctor where id = ?", mapper, new Object[]{ doctorId })
         );
     }
 
     @Override
     public void deleteOne(String doctorId) {
-        String SQL = "DELETE FROM doctors WHERE id = ?";
+        String SQL = "DELETE FROM doctor WHERE id = ?";
         jdbcTemplate.update(SQL, doctorId);
     }
 
     @Override
     public void modify(String id, String firstname, String lastname, String email, String password, String phone, String gender, String speciality) {
-        String SQL = "Update doctors set firstname = ?, lastname = ?, email = ?, password = ?, phone = ?, gender = ?, speciality = ? where id = ?";
+        String SQL = "Update doctor set firstname = ?, lastname = ?, email = ?, password = ?, phone = ?, gender = ?, speciality = ? where id = ?";
         jdbcTemplate.update(SQL, firstname, lastname, email, password, phone, gender, speciality, id);
     }
 
